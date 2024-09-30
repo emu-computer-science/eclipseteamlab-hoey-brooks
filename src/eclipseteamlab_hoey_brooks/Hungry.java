8f5e96de28dacf554eaaad237e580a8cdb99a444
ConnorHere <165110625+ConnorHere@users.noreply.github.com>
2024-09-30 14:24:18 -0400

    Added imHungry method to Hungry class

--- src/eclipseteamlab_hoey_brooks/Hungry.java
+++ src/eclipseteamlab_hoey_brooks/Hungry.java
@@ -4,4 +4,8 @@ public class Hungry {
 	public static void hungry() {
 		System.out.println("We are all hungry!");
 	}
+	
+	public static void imHungry() {
+		System.out.println("I'm really hungry!!");
+	}
 }
--- src/eclipseteamlab_hoey_brooks/driver.java
+++ src/eclipseteamlab_hoey_brooks/driver.java
@@ -7,6 +7,7 @@ public class driver {
 		
 		Hungry.hungry();
 		Sad.sad();
+		Hungry.imHungry();
 	}
 
 }
