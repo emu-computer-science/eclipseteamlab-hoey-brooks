e33b2c9425622a5208aa8aaaee25c0d317e4762c
ConnorHere <165110625+ConnorHere@users.noreply.github.com>
2024-09-30 14:10:24 -0400

    Added hungry class

---
+++ src/eclipseteamlab_hoey_brooks/Hungry.java
@@ -0,0 +1,7 @@
+package eclipseteamlab_hoey_brooks;
+
+public class Hungry {
+	public static void hungry() {
+		System.out.println("We are all hungry!");
+	}
+}
--- src/eclipseteamlab_hoey_brooks/driver.java
+++ src/eclipseteamlab_hoey_brooks/driver.java
@@ -4,6 +4,8 @@ public class driver {
 	
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started");
+		
+		Hungry.hungry();
 	}
 
 }
